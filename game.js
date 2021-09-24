52387374502019d8784cd219f18941c1ea402b72
Carly Collums <carlycollums@gmail.com>
2021-09-23 18:39:19 -0600

    Add half working winning boards recognition

--- game.js
+++ game.js
@@ -3,9 +3,11 @@ class Game {
   this.player1 = new Player(1, 'pumpkin');
   this.player2 = new Player(2, 'ghost');
   this.turn = this.player1;
-  this.board = [];
+  this.boardPlayer1 = [];
+  this.boardPlayer2 = [];
   this.isAWin = false;
   this.draw = false;
+  this.winner = null;
   }
 
   addToPlayerWins(winner) {
@@ -25,29 +27,46 @@ class Game {
   }
 
   winningBoards() {
-    if (this.board.includes(1,2,3)) {
+      if (this.boardPlayer1.includes(1 && 2 && 3) || this.boardPlayer2.includes(1 && 2 && 3)) {
       this.isAWin = true;
-    } else if (this.board.includes(4,5,6)) {
+    } else if (this.boardPlayer1.includes(4 && 5 && 6) || this.boardPlayer2.includes(4 && 5 && 6)) {
       this.isAWin = true;
-    } else if (this.board.includes(7,8,9)) {
+    } else if (this.boardPlayer1.includes(7 && 8 && 9) || this.boardPlayer2.includes(7 && 8 && 9)) {
       this.isAWin = true;
-    } else if (this.board.includes(1,4,7)) {
+    } else if (this.boardPlayer1.includes(1 && 4 && 7) || this.boardPlayer2.includes(1 && 4 && 7)) {
       this.isAWin = true;
-    } else if (this.board.includes(2,5,8)) {
+    } else if (this.boardPlayer1.includes(2 && 5 && 8) || this.boardPlayer2.includes(2 && 5 && 8)) {
       this.isAWin = true;
-    } else if (this.board.includes(3,6,9)) {
+    } else if (this.boardPlayer1.includes(3 && 6 && 9) || this.boardPlayer2.includes(3 && 6 && 9)) {
       this.isAWin = true;
-    } else if (this.board.includes(1,5,9)) {
+    } else if (this.boardPlayer1.includes(1 && 5 && 9) || this.boardPlayer2.includes(1 && 5 && 9)) {
       this.isAWin = true;
-    } else if (this.board.includes(7,5,3)) {
+    } else if (this.boardPlayer1.includes(7 && 5 && 3) || this.boardPlayer2.includes(7 && 5 && 3)) {
       this.isAWin = true;
-    }
+      }
   }
 
-  boardDraw() {
-    if (this.board.includes(1,2,3,4,5,6,7,8,9)) {
-      this.draw = true;
+  // boardDraw() {
+  //   if (this.board.includes(1,2,3,4,5,6,7,8,9)) {
+  //     this.draw = true;
+  //   }
+  // }
+
+  checkForWinner() {
+    this.winningBoards()
+    console.log('player1', this.boardPlayer1)
+    console.log('player2', this.boardPlayer2)
+    console.log(this.isAWin)
+    if (this.isAWin === true) {
+      this.winner = this.turn;
     }
+    console.log(this.winner);
   }
 
+  stopGame() {
+    if (this.winner === this.player1 || this.winner === this.player2) {
+
+  }
+
+}
 }
--- main.js
+++ main.js
@@ -2,15 +2,7 @@ var game = new Game();
 
 // QuerySelectors
 var gameBoard = document.getElementById('gameBoard');
-var box1 = document.querySelector('.box1');
-var box2 = document.querySelector('.box2');
-var box3 = document.querySelector('.box3');
-var box4 = document.querySelector('.box4');
-var box5 = document.querySelector('.box5');
-var box6 = document.querySelector('.box6');
-var box7 = document.querySelector('.box7');
-var box8 = document.querySelector('.box8');
-var box9 = document.querySelector('.box9');
+
 // EventListeners
 gameBoard.addEventListener('click', placeToken);
 
@@ -18,41 +10,58 @@ gameBoard.addEventListener('click', placeToken);
 
 function placeToken(event) {
   if (event.target.classList.contains('1')) {
-    console.log(1);
+    playerboardToSave(1);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('2')) {
-    console.log(2);
+    playerboardToSave(2);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('3')) {
-    console.log(3);
+    playerboardToSave(3);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('4')) {
-    console.log(4);
+    playerboardToSave(4);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('5')) {
-    console.log(5);
+    playerboardToSave(5);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('6')) {
-    console.log(6);
+    playerboardToSave(6);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('7')) {
-    console.log(7);
+    playerboardToSave(7);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('8')) {
-    console.log(8);
+    playerboardToSave(8);
+    game.checkForWinner();
     tokenToBePlaced();
   } else if (event.target.classList.contains('9')) {
-    console.log(9);
+    playerboardToSave(9);
+    game.checkForWinner();
     tokenToBePlaced();
   }
 }
 
-function tokenToBePlaced(boxNumber) {
+function tokenToBePlaced() {
   var addToThisBox = event.target.closest('.box');
-  game.switchTurns();
   if (game.turn === game.player1) {
     addToThisBox.innerHTML += `<h1 class='token'>🎃</h1>`
   } else if (game.turn === game.player2) {
     addToThisBox.innerHTML += `<h1 class='token'>👻</h1>`
+  }
+  game.switchTurns();
+}
+
+function playerboardToSave(boxNumber) {
+  if (game.turn === game.player1) {
+    game.boardPlayer1.push(boxNumber)
+  } else if (game.turn === game.player2) {
+    game.boardPlayer2.push(boxNumber)
   }
 }
